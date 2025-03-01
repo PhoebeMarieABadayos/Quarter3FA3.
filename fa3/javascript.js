cf081a219902f073fc3b4f88277138c8d5dca509
PhoebeMarieABadayos <phoebemarie.badayos@smc.pshs.edu.ph>
2025-03-02 00:08:30 +0800

    Update javascript.js

--- fa3/javascript.js
+++ fa3/javascript.js
@@ -1,12 +1,12 @@
 function generateOrder() {
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let nthLetter = alphabet[firstNum - 1] || "Z";
     let firstNum = Math.ceil(Math.random() * 20);
     let secondNum = Math.ceil(Math.random() * 20);
     let thirdNum = Math.ceil(Math.random() * 20) 
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let nthLetter = alphabet[firstNum - 1] || "Z";
     let highestValue = Math.max(firstNum, secondNum, thirdNum);
-    let hours = Math.floor(totalDuration / 60);
     let totalDuration = secondNum * thirdNum;
+    let hours = Math.floor(totalDuration / 60);
     let minutes = totalDuration % 60;
     let order = "A customer whose name starts with " + "'" + nthLetter + "'" + " have ordered " + highestValue + " times in Papa's Pizzeria. In total, they have stayed in the restaurant for " + hours + " hours and " + minutes + " minutes (" + totalDuration + " minutes).";
 
